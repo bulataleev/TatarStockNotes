72d2faf7e609baa9d759011a8984794a724bc2c4
bulat <b.aleev@mail.ru>
2018-01-21 20:30:12 +0300

    added text info

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,25 @@
-class HomeController < ApplicationController
+class HomeController < ApplicationController #HomeControler inherits ApplicationConttroller.
   def index
+  	
+  	if params[:id]==""
+  		@nothing = "Please type in the stock!"
+	elsif 
+
+		if params[:id]
+			begin
+				@stock = StockQuote::Stock.quote(params[:id])	
+			rescue StandardError
+				@errorl = "That stock symbol doesn't exist... pleasy try again."	
+			end
+		end		
+
+  		# @stock = StockQuote::Stock.quote(params[:id])
+  	end
+
   end
-  def index
+
+  def about
+  
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   
   get 'home/about'
   # get 'home/index'
+  post '/' => 'home#index'
   root 'home#index'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
